85494703d400e112a651183689c29c905033ee37
jimmydalecleveland@gmail.com <jimmydalecleveland@gmail.com>
2022-12-08 11:24:09 -0700

    configure setup to use modern ES
    This is for only modern browsers that stay evergreen.
    It gets us tree-shaking and code-splitting.

--- dist/animalNoise.js
+++ dist/animalNoise.js
@@ -1,6 +1,4 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 function animalNoise(noise) {
     console.log(noise);
 }
-exports.default = animalNoise;
+export default animalNoise;
--- dist/codeSplitTest.js
+++ dist/codeSplitTest.js
@@ -1,7 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 function codeSplitTest() {
     const result = "I, the codeSplitTest, am here!";
     return result;
 }
-exports.default = codeSplitTest;
+export default codeSplitTest;
